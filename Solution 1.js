6710b624b0b5420b934e87474ebaf291f7d93f9c
Harsh Vinayak Kushwaha <harsuk83@gmail.com>
2024-02-03 04:12:27 +0530

    Initial commit

---
+++ Solution 1.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+function readFileContent(filePath) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                console.log(`Error reading file: ${err.code}: ${err.message}`);
+            } else {
+                console.error(`Error reading file: ${err.message}`);
+            }
+        } else {
+            console.log('File Content:');
+            console.log(data);
+        }
+    });
+}
+
+// Test cases
+readFileContent('test-files/file1.txt');
+readFileContent('test-files/empty-file.txt');
+readFileContent('test-files/nonexistent-file.txt');
